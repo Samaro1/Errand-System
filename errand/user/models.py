1ebd8a230399eaf68bda9adbfd8f0a03120a4b6a
Samaro <asamaroingwu@gmail.com>
2025-10-23 15:56:04 +0100

    Made the sign up and login view

--- errand/user/models.py
+++ errand/user/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    fname= models.CharField(max_length=30, null= True, blank= True)
+    lname=  models.CharField(max_length=30, null= True, blank=True)
+    username= models.CharField(max_length= 20)
+    email= models.EmailField(null= True, blank= True)
+    phone_num= models.CharField(max_length= 11, null=True, blank=True)
+    password= models.CharField()
+    account_num= models.CharField(max_length=10, blank=True,null=True)
+    bank_name= models.CharField(blank=True, null=True)
+
+    def  __str__(self):
+        return f"User {self.first} {self.last} phone number: {self.phone_num}"
+    
+
--- errand/user/views.py
+++ errand/user/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.urls import reverse
+from django.contrib.auth import login, authenticate
+from .models import Customer
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+def signup_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = Customer.objects.create_user(username= username, password= password)
+        login(request,user)
+        return redirect("verify")
+    else:
+        return render(request, "user/signup.html")
+
+def login_view(request):
+    if request.method == "POST":
+        username= request.POST["username"]
+        password= request.POST["password"]
+
+        user= authenticate(request, username= username, password= password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render (request, "user/login.html",{
+                "message": "Invalid credentials."})
+    else:
+        return render(request, "user/login.html")
+
+def verify(request):
+    pass
